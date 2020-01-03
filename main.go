a6ebc9468f54a56a618d8c8cb2a107480d9078e5
Basha Mougamadou <b.mougamadou@criteo.com>
2020-01-03 16:17:40 +0100

    Fix import path

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/criteo-forks/terraform-provisioner-chefsolo/chefsolo"
+	"github.com/criteo-forks/terraform-provisioner-chef-solo/chefsolo"
 	"github.com/hashicorp/terraform/plugin"
 )
 
